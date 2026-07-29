ac933179b8c2407537e2ebc180fee10a65cd1ba7
jian45154 <jian45154@gmail.com>
2026-07-29 14:39:32 +1000

    fix: mirror Bash ANSI-C byte decoding

--- tests/governance/test_claude_flash_gate.py
+++ tests/governance/test_claude_flash_gate.py
@@ -100,6 +100,7 @@ class ClaudeFlashGateTests(unittest.TestCase):
             r"fa$'\x73\x74'boot -s SERIAL flash userdata candidate.img",
             r"fa$'\c@'stboot -s SERIAL flash userdata candidate.img",
             r"fa$'\x00'stboot -s SERIAL flash userdata candidate.img",
+            r"fa$'\400'stboot -s SERIAL flash userdata candidate.img",
             'fa$"st"boot -s SERIAL flash userdata candidate.img',
         )
         for command in commands:
@@ -116,6 +117,7 @@ class ClaudeFlashGateTests(unittest.TestCase):
             "d$'d' if=image of=/dev/mmcblk0",
             r"d$'\x64' if=image of=/dev/mmcblk0",
             r"d$'\c@'d if=image of=/dev/mmcblk0",
+            r"d$'\400'd if=image of=/dev/mmcblk0",
         )
         for command in commands:
             with self.subTest(command=command):
